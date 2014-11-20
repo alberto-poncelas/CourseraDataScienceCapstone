acb5e682299f500874ee78876fbb8e6f348a1dee
Alberto Poncelas <alberto.poncelas@gmail.com>
2014-11-21 00:25:22 +0100

    preprocessing functions added in R script

--- FinalProject/predictingFunctions.R
+++ FinalProject/predictingFunctions.R
@@ -6,6 +6,26 @@ tbl[[2]]<-read.table("grams2Count")
 tbl[[3]]<-read.table("grams3Count")
 
 
+##### (OPTIONAL) PREPROCESSING ########
+
+#Filter ngrams so we have only one (the most probable) prediction for each word/two words
+
+tbl2F<-tbl[[2]]
+tbl2F <- tbl2F[order(tbl2F$V2, -tbl2F$V1 ), ] 	#order by 1st word of 2-gram and then by the number of ocurrences (descending)
+tbl2F<-tbl2F[ !duplicated(tbl2F$V2), ]		#Remove duplicates and just keep the first occurrence
+
+
+tbl3F<-tbl[[3]]
+tbl3F <- tbl3F[order(tbl3F$V2, tbl3F$V3, -tbl3F$V1 ), ]
+tbl3F<-tbl3F[ !duplicated(c(tbl3F$V2,tbl3F$V3)), ]
+
+
+tblF=list()
+tblF[[2]]<-tbl2F
+tblF[[3]]<-tbl3F
+
+
+
 
 ######### PREDICTING FUNCTIONS ##########
 
